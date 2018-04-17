47cd5678e6f697fa40cf1440e3534800f179648b
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-17 15:43:22 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+if array.length == 2 
+  array.join(" and ")
+else if array.length < 2
+  array.join(", ")
+else array.length == 3
+  array[-1].insert(0, "and ")
+  array.join(", ")
+end 
 end
+end 
