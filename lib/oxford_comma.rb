5b8061005841b111b8f9e93f40b26b92e3a245ee
tasneem torkia <tasneemtorkia@hotmail.co.uk>
2018-04-17 15:40:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.length == 2 
   array.join(" and ")
 else if array.length < 2
-  array.join(" ")
+  array.join(" , ")
 else if array.length == 3
   aarray[-1].insert(0, "and ")
 end 
